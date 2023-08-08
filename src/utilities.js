ace8340473d4a783e08b98bc803a6fa6bc795dab
urchifox <126313096+urchifox@users.noreply.github.com>
2023-08-08 14:55:20 +0400

    make tag for validation template strings

---
+++ src/utilities.js
@@ -0,0 +1,22 @@
+/**
+ * @param {TemplateStringsArray} strings
+ * @param {...any} values
+ * @returns {string}
+ */
+function html(strings, ...values) {
+  return strings.reduce((before, after, index) => {
+    const value = values[index - 1];
+
+    if (value === undefined) {
+      return before + after;
+    }
+
+    if (Array.isArray(value)) {
+      return before + value.join('') + after;
+    }
+
+    return before + value + after;
+  });
+}
+
+export {html};
