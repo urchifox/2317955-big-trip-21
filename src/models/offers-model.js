4fe4a9bce6c5b2449a83372ea57f77658e1eea1c
urchifox <126313096+urchifox@users.noreply.github.com>
2023-08-24 15:32:07 +0400

    add to offers and destinations model get- methods

--- src/models/offers-model.js
+++ src/models/offers-model.js
@@ -6,4 +6,23 @@ export default class OffersModel {
   get offers() {
     return this.#offers;
   }
+
+  getOffersByIds(offersIds) {
+    const chosenOffers = [];
+    if (offersIds.length === 0) {
+      return;
+    }
+
+    offersIds.forEach((offerId) => {
+      const chosenOffer = this.#offers.find((offer) => offer.id === offerId);
+      if (chosenOffer) {
+        chosenOffers.push(chosenOffer);
+      }
+    });
+    return chosenOffers;
+  }
+
+  getOffersByType(type) {
+    return this.#offers.filter((offer) => offer.type === type);
+  }
 }
